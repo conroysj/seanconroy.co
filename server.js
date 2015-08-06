2fcdaa9c8607fdbe70863fcdd3719696d4c2b7a7
Sean Conroy <conroysean@gmail.com>
2015-08-06 13:57:31 -0700

    [feat] Changes to  gulp file, index, package.json, server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(__dirname + '/'));
+
+app.get('*', function(request, response) {
+  response.sendFile(path.join(__dirname, './', 'index.html'));
+});
+
+app.listen(port);
+console.log("Server listening on port " + port);
