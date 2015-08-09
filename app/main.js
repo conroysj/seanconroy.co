8c606e99a765ccc8a59f721642c754fe59d49d86
Sean Conroy <conroysean@gmail.com>
2015-08-08 20:05:46 -0700

    [debug] Loads build.js using webpack

--- app/main.js
+++ app/main.js
@@ -1,11 +1,11 @@
 'use strict';
-import component = require('./components/navbar.js');
-import React from 'react';
-import Router from 'react-router';
+import component from "./components/navbar.js";
+import React from "react";
+import Router from "react-router";
 
 window.React = React;
 // var headParams = new HeadParams();
 
-Router.run(routes, Router.HistoryLocation)
+//Router.run(routes, Router.HistoryLocation);
 
 document.body.appendChild(component());
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require('path');
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/build'));
 
 app.get('*', function(request, response) {
   response.sendFile(path.join(__dirname, './', 'index.html'));
